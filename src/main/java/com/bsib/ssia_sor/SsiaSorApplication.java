e6cea7c11cb517d3be210c0d1f195e4fa0afc10e
Blessed Sibanda <blessedsibanda.me@gmail.com>
2022-10-19 13:39:38 +0200

    init project

---
+++ src/main/java/com/bsib/ssia_sor/SsiaSorApplication.java
@@ -0,0 +1,13 @@
+package com.bsib.ssia_sor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsiaSorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsiaSorApplication.class, args);
+	}
+
+}
